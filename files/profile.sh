92752ed77b1bafa54219e3221774f607b4cc1e01
Nicolas Calderon <nicolas.calderon.asselin@gmail.com>
2021-11-23 15:17:10 -0500

    Add profile configuration directory

--- files/profile.sh
+++ files/profile.sh
@@ -1,6 +1,6 @@
 # Add local bin directory to path
 if [ -d "$HOME/local/bin" ]; then
-	PATH="$HOME/local/bin:$PATH"
+	export PATH="$HOME/local/bin:$PATH"
 fi
 
 # --no-init disables clearing the screen after exiting
@@ -10,3 +10,9 @@ export LESS='--no-init --RAW-CONTROL-CHARS'
 # Set default editor
 export EDITOR=vim
 
+
+# Source all file in the .profile.d directory
+for profile in ${HOME}/.profile.d/*;
+do
+	. "$profile"
+done
